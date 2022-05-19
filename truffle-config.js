9e5d6bdf14927eb6ab216479ce9a04a021e1f7c2
ArchitectAK <ankitdroiddeveloper@gmail.com>
2022-05-19 12:22:50 +0800

    Move contrcats under src and update truffle config accordingly

--- truffle-config.js
+++ truffle-config.js
@@ -34,6 +34,8 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+  contracts_directory: "./src/contracts",
+  contracts_build_directory: "./src/build",
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -41,11 +43,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
